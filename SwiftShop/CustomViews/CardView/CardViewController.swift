ea9c42a2179b0ac12b1bc83913c30e7add72edf6
Samiiahmed <eldon.smsm135@gmail.com>
2024-10-14 21:37:59 +0300

    cardView

---
+++ SwiftShop/CustomViews/CardView/CardViewController.swift
@@ -0,0 +1,26 @@
+//
+//  CardViewController.swift
+//  SwiftShop
+//
+//  Created by Sami Ahmed on 14/10/2024.
+//
+
+import UIKit
+
+class CardView: UIView {
+    
+    //MARK: - @IBOUTLETS
+    
+    @IBOutlet weak var cardImageView: UIImageView!
+    @IBOutlet weak var cardName: UILabel!
+    @IBOutlet weak var cardNumberLabel: UILabel!
+    @IBOutlet weak var cardNumber: UILabel!
+    @IBOutlet weak var cardHolderNameLabel: UILabel!
+    @IBOutlet weak var cardHolderName: UILabel!
+    @IBOutlet weak var validThruLabel: UILabel!
+    @IBOutlet weak var validThrouDate: UILabel!
+    
+    
+    
+    
+}
